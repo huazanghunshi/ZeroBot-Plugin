6f24f65b935144e8960491ae4f303a7af930fbca
huazanghunshi <96936217+huazanghunshi@users.noreply.github.com>
2022-03-07 12:50:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -239,11 +239,11 @@ func main() {
 		})
 	zero.Run(
 		zero.Config{
-			NickName:      append([]string{*adana}, nicks...),
+			NickName:      append([]string{"ゆい"}, nicks...),
 			CommandPrefix: *prefix,
 			// SuperUsers 某些功能需要主人权限，可通过以下两种方式修改
-			// SuperUsers: []string{"12345678", "87654321"}, // 通过代码写死的方式添加主人账号
-			SuperUsers: flag.Args(), // 通过命令行参数的方式添加主人账号
+			SuperUsers: []string{"2015049960", "1984396105"}, // 通过代码写死的方式添加主人账号
+			// SuperUsers: flag.Args(), // 通过命令行参数的方式添加主人账号
 			Driver:     []zero.Driver{driver.NewWebSocketClient(*url, *token)},
 		},
 	)
